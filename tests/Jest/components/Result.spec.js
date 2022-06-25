4df6cc9bf8c159adb875592e4252916788a5972f
Christian Dangl <boxblinkracer@gmx.net>
2022-06-26 00:05:13 +0200

    add jest test for result class

---
+++ tests/Jest/components/Result.spec.js
@@ -0,0 +1,17 @@
+import Result from "../../../src/components/TestRail/Result";
+
+
+test('caseId is correctly assigned', () => {
+    const result = new Result('C123', 1, 'this is a test');
+    expect(result.getCaseId()).toBe('C123');
+});
+
+test('status is correctly assigned', () => {
+    const result = new Result('C123', 1, 'this is a test');
+    expect(result.getStatusId()).toBe(1);
+});
+
+test('comment is correctly assigned', () => {
+    const result = new Result('C123', 1, 'this is a test');
+    expect(result.getComment()).toBe('this is a test');
+});
