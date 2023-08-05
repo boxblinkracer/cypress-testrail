1dabfcaab359ebc5107a0d3a21310b8e71b4adb7
Aditya <adyfbd@hotmail.com>
2023-08-05 18:57:13 +0200

    Update TestRail.js

--- src/components/TestRail/TestRail.js
+++ src/components/TestRail/TestRail.js
@@ -289,7 +289,7 @@ class TestRail {
             url,
             postData,
             (response) => {
-                ColorConsole.success(' Results sent to TestRail for: ' + testResults.map((r) => 'C' + r.getCaseId()));
+                ColorConsole.success(' Results sent to TestRail for run R' + runID + ': ' + testResults.map((r) => 'C' + r.getCaseId()));
 
                 if (this.isScreenshotsEnabled) {
                     const allRequests = [];
